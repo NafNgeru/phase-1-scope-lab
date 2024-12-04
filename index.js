2551ead42d4b121f73270912e0b9b7ac977b7ed6
Naftaly Ngeru <n.ngeru.k@gmail.com>
2024-12-04 14:40:20 +0300

    Completed Labs

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function customerUpperCase(customerName){
+    return customerName.toUpperCase();
+}
+customerUpperCase(customerName);
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob';
+    return bestCustomer;
+}
+setBestCustomer();
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+overwriteBestCustomer();
+
+const leastFavoriteCustomer = 'me';
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'someone else';
+}
+changeLeastFavoriteCustomer();
+ 
